2f0a3ac9fdf6c97b7133ac63c47c96239b4f18ea
dahoon06 <dahoon226@naver.com>
2023-01-12 23:21:33 +0900

    chore: image 주석

--- client/next.config.js
+++ client/next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: "@import '@styles/utils/colors.scss';",
   },
+  images: {
+    domains: ['picsum.photos'],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- client/src/components/posts/PostLists.tsx
+++ client/src/components/posts/PostLists.tsx
@@ -2,14 +2,18 @@ import styled from '@styles/posts/posts.module.scss'
 import { Tags } from '@components/tag/Tags'
 import { Typography } from '@components/common/typography/Typography'
 import cn from 'classnames'
+import Image from 'next/image'
 
 export const PostLists = () => {
+  const url = 'https://picsum.photos/300/300'
   return (
     <article className={styled.post__container}>
       <section className={styled.post__wrapper}>
-        <div className={'w-100'}>
-          <img src={'https://picsum.photos/300/300'} alt={'thumbnail'} />
-        </div>
+        {/*<div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div><div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div>*/}
         <Typography
           variant={'h1'}
           fontColor={'black'}
@@ -38,50 +42,102 @@ export const PostLists = () => {
         <Tags />
       </section>
       <section className={styled.post__wrapper}>
-        <div>THUMBNAIL</div>
-        <div>TITLE</div>
-        <div>CREATED_DATE</div>
-        <div>DESCRIPTION</div>
+        {/*<div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div><div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div>*/}
+        <Typography
+          variant={'h1'}
+          fontColor={'black'}
+          fontWeight={'semibold'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          TITLE
+        </Typography>
+
+        <Typography
+          variant={'caption'}
+          fontColor={'gray2'}
+          fontWeight={'medium'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          Jul. 12. 2023
+        </Typography>
+        <Typography
+          variant={'caption'}
+          fontColor={'gray2'}
+          fontWeight={'medium'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          Let's load the sitemap
+        </Typography>
         <Tags />
       </section>
       <section className={styled.post__wrapper}>
-        <div className={'w-100'}>
-          <img src={'https://picsum.photos/300/300'} alt={'thumbnail'} />
-        </div>
-        <div>
-          <Typography
-            variant={'h2'}
-            fontColor={'black'}
-            fontWeight={'semibold'}
-          >
-            TITLE
-          </Typography>
-        </div>
-        <div>
-          <Typography
-            variant={'caption'}
-            fontColor={'gray2'}
-            fontWeight={'medium'}
-          >
-            Jul. 12. 2023
-          </Typography>
-        </div>
-        <div>
-          <Typography
-            variant={'caption'}
-            fontColor={'gray2'}
-            fontWeight={'medium'}
-          >
-            Let's load the sitemap
-          </Typography>
-        </div>
+        {/*<div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div><div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div>*/}
+        <Typography
+          variant={'h1'}
+          fontColor={'black'}
+          fontWeight={'semibold'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          TITLE
+        </Typography>
+
+        <Typography
+          variant={'caption'}
+          fontColor={'gray2'}
+          fontWeight={'medium'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          Jul. 12. 2023
+        </Typography>
+        <Typography
+          variant={'caption'}
+          fontColor={'gray2'}
+          fontWeight={'medium'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          Let's load the sitemap
+        </Typography>
         <Tags />
       </section>
       <section className={styled.post__wrapper}>
-        <div>THUMBNAIL</div>
-        <div>TITLE</div>
-        <div>CREATED_DATE</div>
-        <div>DESCRIPTION</div>
+        {/*<div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div><div className={'w-100'}>*/}
+        {/*  <Image width={300} height={300} src={url} alt={'thumbnail'} />*/}
+        {/*</div>*/}
+        <Typography
+          variant={'h1'}
+          fontColor={'black'}
+          fontWeight={'semibold'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          TITLE
+        </Typography>
+
+        <Typography
+          variant={'caption'}
+          fontColor={'gray2'}
+          fontWeight={'medium'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          Jul. 12. 2023
+        </Typography>
+        <Typography
+          variant={'caption'}
+          fontColor={'gray2'}
+          fontWeight={'medium'}
+          className={cn(styled.post__label, 'w-100')}
+        >
+          Let's load the sitemap
+        </Typography>
         <Tags />
       </section>
     </article>
