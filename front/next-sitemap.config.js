82c7e11ec94e61a88e90a9be0f84004442d4af32
dahoon06 <dahoon226@naver.com>
2023-01-13 10:48:48 +0900

    이전 블로그 파일 삭제

--- front/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = require('./morethan-log.config')
-
-module.exports = {
-  siteUrl: CONFIG.link,
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  generateIndexSitemap: false
-}
--- front/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: false,
-  images: {
-    loader: 'custom',
-  },
-}
