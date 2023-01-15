790b8038375283161b572a580de35ace7666d321
dahoon06 <dahoon226@naver.com>
2023-01-15 22:23:53 +0900

    chore: build 에러 수정

--- client/src/lib/posts/index.ts
+++ client/src/lib/posts/index.ts
@@ -1,20 +1,17 @@
 import * as fs from "fs";
 
-const postDirectory = require('../../../posts/test.md');
 
 const getAllPostsIds = () => {
-  const fileNames = fs.readdirSync(postDirectory)
-  console.log(fileNames);
 
-  return fileNames.map((fileName) => {
-      return (
-        {
-          params: {
-            id: fileName.replace(/\.md$/, ''),
-          }
-        }
-      )
-  })
+  // return fileNames.map((fileName) => {
+  //     return (
+  //       {
+  //         params: {
+  //           id: fileName.replace(/\.md$/, ''),
+  //         }
+  //       }
+  //     )
+  // })
 }
 
 export default getAllPostsIds();
