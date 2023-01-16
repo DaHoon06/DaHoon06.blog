b8e18c2ee7ee05b61fb83ccf46bddf93ca94ff8a
dahoon06 <dahoon226@naver.com>
2023-01-17 00:15:05 +0900

    fix: next config md loader 설정
    init raw-loader

--- client/next.config.js
+++ client/next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: "@import '@styles/utils/colors.scss';",
   },
+  webpack: config => {
+    config.module.rules.push(
+      {
+        test: /\.md$/,
+        use: "raw-loader"
+      },
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
--- client/src/components/posts/index.tsx
+++ client/src/components/posts/index.tsx
@@ -1,18 +1,36 @@
 import { PostLists } from '@components/posts/PostLists'
 import { Typography } from '@components/common/typography/Typography'
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import style from '@styles/posts/posts.module.scss'
+import test from '../../../../posts/test.md';
 
 import cn from 'classnames'
 import { SearchInput } from "@components/search/SearchInput";
+import axios from "axios";
 
 export const PostContainer = () => {
   const [count, setCount] = useState<number>(7)
 
+  useEffect(() => {
+    const data = JSON.stringify(test)
+    console.log(data);
+  })
+
+
+
+  const apiTest = async () => {
+    try {
+      const { data } = await axios.get('https://github.com/DaHoon06/DaHoon06.blog/blob/main/posts/test.md');
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <>
       <section className={style.post__index}>
         <SearchInput />
+        <button type={'button'} onClick={apiTest}>TEST</button>
         <div
           className={cn(style.post__wrapper__label, 'flex-justify-between')}
         >
