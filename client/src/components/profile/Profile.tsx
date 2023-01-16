9466964cc2a2dd31443872b40f09ab3ef070da4b
dahoon06 <dahoon226@naver.com>
2023-01-17 00:19:22 +0900

    fix: 이미지 priority 경고 수정

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -15,7 +15,7 @@ export const Profile = () => {
       <section>
         <section className={style.profile__wrapper}>
           <div className={style.profile}>
-            <Image src={PROFILE} alt={'profile'} width={100} height={100} />
+            <Image loading="eager" priority={true} src={PROFILE} alt={'profile'} width={100} height={100} />
           </div>
           <Typography variant={'sub1'} fontColor={'black'} fontWeight={'medium'}>
             {CONFIG.profile.name}
@@ -27,14 +27,14 @@ export const Profile = () => {
 
         <section className={style.profile__information}>
           <div className={'flex-align-center pb-8'}>
-            <Image src={EMAIL} alt={'profile'} width={18} height={18} />
+            <Image loading="eager" priority={true} src={EMAIL} alt={'profile'} width={18} height={18} />
             <Typography className={'pl-8'} variant={'caption'} fontColor={'black'}>
               {CONFIG.profile.email}
             </Typography>
           </div>
 
           <div className={'flex-align-center pb-8'}>
-            <Image src={GITHUB} alt={'profile'} width={18} height={18} />
+            <Image loading="eager" priority={true} src={GITHUB} alt={'profile'} width={18} height={18} />
             <Link href={CONFIG.link.github}>
               <Typography className={'pl-8'} variant={'caption'} fontColor={'black'}>
                 {CONFIG.profile.github}
@@ -43,7 +43,7 @@ export const Profile = () => {
           </div>
 
           <div className={'flex-align-center pb-8'}>
-            <Image src={TWITTER} alt={'profile'} width={18} height={18} />
+            <Image loading="eager" priority={true} src={TWITTER} alt={'profile'} width={18} height={18} />
             <Link href={CONFIG.link.twitter}>
               <Typography className={'pl-8'} variant={'caption'} fontColor={'black'}>
                 {CONFIG.profile.twitter}
